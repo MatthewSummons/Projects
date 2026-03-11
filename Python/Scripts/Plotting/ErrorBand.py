d08a57915c60a2878dc2959e6ed025b5db866ffa
Shaheer Ziya <46129554+MatthewSummons@users.noreply.github.com>
2026-03-11 12:37:13 +0800

    Scripts For Plotting Error Bands

---
+++ Python/Scripts/Plotting/ErrorBand.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.polynomial import Polynomial
+import seaborn as sns
+
+ORANGE = '#D57541'
+BLUE   = '#2E5F7F'
+
+sns.set_theme()
+N = 100
+x = np.linspace(start=0.0, stop=0.175, num=N)
+
+r, r_err = 0.0275, 0.001    # Inner Radius of Cylinder (± error)
+R, R_err = 0.0315, 0.001    # Outer Radius of Cylinder (± error)
+g = 9.81                    # Acceleration due to Gravity
+
+
+def omega(h:float | np.ndarray, r:float, R:float) -> float | np.ndarray:
+    return np.sqrt((4 * g * h) / (3 * R ** 2 + r ** 2))
+
+
+def omega_sqr(h:float | np.ndarray, r:float, R:float) -> float | np.ndarray:
+    return (4 * g * h) / (3 * R ** 2 + r ** 2)
+
+
+lower_bounds = omega(x, r + r_err, R + R_err)
+theoretical = omega(x, r, R)
+upper_bounds = omega(x, r - r_err, R - R_err)
+
+# Experimental Data (Height in m, Angular Frequency (& its square) in rad/s, and their respective errors)
+x_data, err_x    = [0.03, 0.06, 0.09, 0.12, 0.15],             [0.002] * 5
+y, err_y         = [12.08, 21.67, 28.56, 34.27, 36.96],        [0.42, 1.49, 1.30, 2.80, 2.17]
+y_sqr, sqr_err_y = [146.00, 469.42, 815.67, 1174.56, 1366.03], [5.62, 64.75, 74.15, 192.20, 160.71]
+
+# Least-Squares Fits for Experimental Data
+series_fit_w = Polynomial.fit(x_data, y, 2)
+best_fit_y = series_fit_w(x)
+
+
+series_fit_w_sqr = Polynomial.fit(x_data, y_sqr, 1)
+best_fit_y_sqr = series_fit_w_sqr(x)
+
+
+# Figure 1
+plt.figure("Angular Frequency on Height", figsize=(10, 6))
+
+plt.plot(x, theoretical, '-', color="ORANGE", label='Theoretical')
+plt.fill_between(x, lower_bounds, upper_bounds, color=ORANGE, alpha=0.2)
+
+plt.errorbar(x_data, y, xerr=err_x, yerr=err_y, capsize=3, ms=5, color=BLUE, linestyle='None')
+plt.plot(x, best_fit_y, '--', color=BLUE, alpha=0.5, label='Experimental')
+
+plt.xlabel("Vertical Height (m)")
+plt.ylabel("Angular Frequency ($rads^{-1}$)")
+plt.legend(title='Angular Frequency on Height')
+
+# Figure 2
+plt.figure("Squared Angular Frequency on Height", figsize=(10, 6))
+plt.plot(x, omega_sqr(x, r, R), '-', color='#D57541', label='Theoretical')
+plt.fill_between(x, omega_sqr(x, r + r_err, R + R_err), omega_sqr(x, r - r_err, R - R_err), color=ORANGE, alpha=0.2)
+
+plt.errorbar(x_data, y_sqr, xerr=err_x, yerr=sqr_err_y, capsize=3, ms=5, color=BLUE, linestyle='None')
+plt.plot(x, best_fit_y_sqr, '--', color=BLUE, alpha=0.5, label='Experimental')
+plt.xlabel("Vertical Height (m)")
+plt.ylabel("Squared Angular Frequency ($rad^2s^{-2}$)")
+plt.legend(title='Squared Angular Frequency on Height')
+
+plt.show()
---
+++ Python/Scripts/Plotting/derivation.md
@@ -0,0 +1,23 @@
+## The Model 
+
+A thick cylinder with inner radius $r$ and and outer radius $R$ is release from rest at a vertical height $h$ on a rough inclined plane.
+
+The cylinder rolls down the plane withot slipping or sliding, and the period for one whole rotation is recorded with varying heights. 
+
+The experiment is repeated for 3 times at each height, and the average period is calculated.
+
+The angular frequency $\omega$ of the cylinder is then calculated with the formula $\omega = \frac{2\pi}{T}$, where $T$ is the period of one whole rotation.
+
+The angular frequency  of the cylinder is measured at different heights. The theoretical model for the angular frequency can derived as a function of height $h$ and the inner and outer radius of the cylinder $r$ and $R$ as follows.
+
+The moment of inertia of the cylinder is given by $I = \frac{1}{2}m(R^2 + r^2)$, where $m$ is the mass of the cylinder. The potential energy at height $h$ is $U = mgh$, the kinetic energy at the bottom of the incline is $K = \frac{1}{2}mv^2 + \frac{1}{2}I\omega^2$.
+
+By conservation of energy, we have $mgh = \frac{1}{2}mv^2 + \frac{1}{2}I\omega^2$.
+
+Since the cylinder rolls without slipping, we have $v = \omega R$. Therefore, the kinetic energy can be rewritten as $K = \frac{1}{2}m(\omega R)^2 + \frac{1}{2}I\omega^2$.
+
+
+Substituting the expression for $I$ and rearranging, we can derive the formula for the angular frequency $\omega$ as a function of height $h$, inner radius $r$, and outer radius $R$:
+
+$$\omega^2 = \frac{4gh}{3R^2 + r^2}$$
+
